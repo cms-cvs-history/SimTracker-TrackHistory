a4001690fb8f5c3002058ce61a1d94e6e80fa259
Victor Eduardo Bazterra <sha1-54ef19053215ea8475b12680ac86ec0eec09114f@cern.ch>
2007-09-25 00:49:26 +0000

    Some menor bug fixing testing in 1_6_0.

--- test/TruthTOA.C
+++ test/TruthTOA.C
@@ -1,13 +1,18 @@
 {
 
-TFile file("test.root");
+   gROOT->SetStyle("Plain");
 
-TCanvas c1;
-TPie * pie = (TPie*) gDirectory->Get("TrackingParticleCollection");
-pie->Draw();
+   TFile file("test.root");
 
-TCanvas c2;
-pie = (TPie*) gDirectory->Get("TrackingParticleCollection_pi+");
-pie->Draw();
+   TCanvas * c1 = new TCanvas;
+
+   TH1D * hist = (TH1D*) gDirectory->Get("TrackingParticleCollection");
+   hist->SetFillColor(49);
+   hist->Draw("hbar2");
+
+   TCanvas * c2 = new TCanvas;
+   hist = (TH1D*) gDirectory->Get("TrackingParticleCollection_pi+");
+   hist->SetFillColor(49);
+   hist->Draw("hbar2"); 
 
 }
